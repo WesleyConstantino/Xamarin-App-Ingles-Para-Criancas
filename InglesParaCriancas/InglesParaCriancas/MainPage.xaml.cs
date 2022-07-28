7a31e212b93ebe5cba7a2e96d79f49659eacf998
x601533 <wesleycs@prodam.sp.gov.br>
2022-07-28 14:10:40 -0300

    Adicionar arquivos de projeto.

---
+++ InglesParaCriancas/InglesParaCriancas/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InglesParaCriancas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btCores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Cores());
+        }
+
+        private void btNumeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Numeros());
+        }
+
+        private void btFamilia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Familia());
+        }
+
+        private void btSair_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
